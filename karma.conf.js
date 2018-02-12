d18ca7b2a9cfc6e762ba0028a13f43ffc72e35dc
Jose Almeida <jose@MacBook-Pro.hitronhub.home>
2018-02-12 01:46:20 -0800

    Add test components

---
+++ karma.conf.js
@@ -0,0 +1,23 @@
+var webpackConfig = require('./webpack.config.js');
+
+module.exports = function (config){
+    config.set({
+        browsers:['Chrome'],
+        singleRun: true,
+        framework:['mocha'],
+        files: ['app/tests/**/*.test.jsx'],
+        preprocessors: {
+            'app/tests/**/*.test.jsx': ['webpack','sourcemap']
+        },
+        reporters: ['mocha'],
+        clients:{
+            mocha:{
+                timeout: '5000'
+            }
+        },
+        webpack: webpackConfig,
+        webpackServer: {
+            noInfo:true
+        }
+    });
+};
